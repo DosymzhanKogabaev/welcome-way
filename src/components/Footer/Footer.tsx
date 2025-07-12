69841c54c052acbbcdf7df4169ac3f2d27bd2be4
Maksym Yaroshenko <yiaroshenko.max@gmail.com>
2025-07-12 05:17:04 +0300

    layout Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,62 @@
+// src/components/Footer/Footer.tsx
+
+import style from './Footer.module.css';
+
 export const Footer: React.FC = () => {
-  return <></>;
+  const footerList = [
+    {
+      name: 'About Us',
+      about: ['About', 'Mission', 'Team', 'Volunteer', 'Donate'],
+    },
+    {
+      name: 'Get Involved',
+      about: [
+        'Contact Us',
+        'FAQ',
+        'Terms of Use',
+        'Privacy Policy',
+        'Cookie Policy',
+      ],
+    },
+    {
+      name: 'Connect with Us',
+      about: ['Facebook', 'X', 'LinkedIn', 'Youtube'],
+    },
+  ];
+
+  return (
+    <footer className={`${style.footer} container`}>
+      <h2 className={style.tytle}>WelcomeWay</h2>
+      <h3>
+        Subscribe to our newsletter for the latest updates on new features and
+        product releases.
+      </h3>
+      <input
+        className={style.emailInput}
+        type="email"
+        placeholder="Enter your email"
+      />
+      <button className={style.submitEmailBtn} type="submit">
+        Subscribe to Newsletter
+      </button>
+      <span className={style.license}>
+        © 2023 WelcomeWay. All Rights Reserved.
+      </span>
+
+      <ul className={style.footerList}>
+        {footerList.map((section, index) => (
+          <li key={index} className={style.footerItem}>
+            <h4 className={style.sectionTitle}>{section.name}</h4>
+            <ul className={style.linksList}>
+              {section.about.map((item, idx) => (
+                <li key={idx} className={style.linkItem}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 };
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,12 +4,15 @@ import React from 'react';
 import styles from './HomePage.module.css';
 import '../../index.css';
 import { Header } from '../../components/Header/Header';
+import { Footer } from '../../components/Footer/Footer';
 
 export const HomePage: React.FC = () => {
   return (
     <section className={`${styles.homeSection} section`}>
       <div className={`${styles.homeContainer} container`}>
         <Header />
+
+        <Footer />
       </div>
     </section>
   );
