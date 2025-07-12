d21e7fa9d91dc91d7be3b4a0c70d2691d4929ddc
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2025-07-12 12:50:28 +0200

    NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,25 @@
 // src/pages/NotFoundPage/NotFoundPage.tsx
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './NotFoundPage.module.css';
 import '../../index.css';
 
 export const NotFoundPage: React.FC = () => {
   return (
     <section className={`${styles.homeSection} section`}>
-      <div className={`${styles.homeContainer} container`}></div>
+      <div className={`${styles.homeContainer} container`}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>404</h1>
+          <p className={styles.subtitle}>Oops! Page Not Found</p>
+          <p className={styles.message}>
+            It seems you've wandered off the path. Let's get you back on track!
+          </p>
+          <Link to="/" className={styles.backButton}>
+            Back to Home
+          </Link>
+        </div>
+      </div>
     </section>
   );
 };
