86b16b936a9d5730337f7a5233501d024edf32b1
Dosymzhan Kogabaev <dkogabayev@mail.ru>
2025-07-13 18:02:02 +0500

    fixes

--- worker/middlewares/cdn.ts
+++ worker/middlewares/cdn.ts
@@ -37,8 +37,7 @@ export const cdnMiddleware = async (
   }
 
   const headers = new Headers({
-    'Content-Type':
-      object.httpMetadata?.contentType || 'application/octet-stream',
+    'Content-Type': object.httpMetadata?.contentType || inferFromKey(key),
     'Cache-Control': 'public, max-age=86400, immutable',
     ETag: etag,
     'Content-Length': object.size?.toString() ?? '',
@@ -52,4 +51,20 @@ export const cdnMiddleware = async (
   }
 
   return new Response(object.body, { headers });
+};
+
+const inferFromKey = (key: string) => {
+  if (key.endsWith('.png')) {
+    return 'image/png';
+  }
+  if (key.endsWith('.jpg') || key.endsWith('.jpeg')) {
+    return 'image/jpeg';
+  }
+  if (key.endsWith('.gif')) {
+    return 'image/gif';
+  }
+  if (key.endsWith('.svg')) {
+    return 'image/svg+xml';
+  }
+  return 'application/octet-stream';
 };
